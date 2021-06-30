65f8d91a77f62838ef585b7730ed58243de284ca
Felipe Moreira <fmoreira95@hotmail.com>
2021-06-30 17:26:59 -0400

    quotes container built, home mobile view

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 const aboutContainer = document.getElementById("about__container");
+const quotesContainer = document.getElementById("quotes__container");
+console.log(quotesContainer);
 
 let details = [
   {
@@ -23,6 +25,33 @@ let details = [
   },
 ];
 
+let quotes = [
+  {
+    quoteImg: "../assets/icon-quotes.svg",
+    description:
+      "“The team perfectly fit the specialized skill set required. They focused on the most essential features helping us launch the platform eight months faster than planned.”",
+    name: "Kady Baker",
+    position: "Product Manager at Bookmark",
+    image: "../assets/avatar-kady.jpg",
+  },
+  {
+    quoteImg: "../assets/icon-quotes.svg",
+    description:
+      "“We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!”",
+    name: "Aiysha Reese",
+    position: "Founder of Manage",
+    image: "../assets/avatar-aiysha.jpg",
+  },
+  {
+    quoteImg: "../assets/icon-quotes.svg",
+    description:
+      "“Amazing. Our team helped us build an app that delivered a new experience for hiring a physio. The launch was an instant success with 100k downloads in the first month.”",
+    name: "Arthur Clarke",
+    position: "Co-founder of MyPhysio",
+    image: "../assets/avatar-arthur.jpg",
+  },
+];
+
 const renderInfo = (arr) => {
   const details = arr
     .map((detail) => {
@@ -39,3 +68,22 @@ const renderInfo = (arr) => {
   aboutContainer.innerHTML = details;
 };
 renderInfo(details);
+
+const renderQuotes = (arr) => {
+  const quotes = arr
+    .map((quote) => {
+      return `
+                <div class="quote__container">
+                <img class="quote__container--quoteImg" src="${quote.quoteImg}" alt="quote-img"/>
+                <p class="quote__container--description">${quote.description}</p>
+                <h2 class="quote__container--title">${quote.name}</h2>
+                <p class="quote__container--position">${quote.position}</p>
+                <img class="quote__container--svg" src="${quote.image}" alt="image"/>
+                </div>
+        `;
+    })
+    .join("");
+
+  quotesContainer.innerHTML = quotes;
+};
+renderQuotes(quotes);
