f844d58a634f775c2b20d680d5798adcb05d5924
Felipe Moreira <fmoreira95@hotmail.com>
2021-07-07 19:41:00 -0400

    working on about us functionality

--- scripts/about.js
+++ scripts/about.js
@@ -8,30 +8,60 @@ const directorsArr = [
     name: "Nikita Marks",
     role: "Founder & CEO",
     icon: "../assets/icon-cross.svg",
+    quote:
+      "It always amazes me how much talent there is in every corner of the globe.",
+    twitterIcon: "../assets/icon-twitter.svg",
+    linkedinIcon: "../assets/icon-linkedin.svg",
   },
   {
     image: "../assets/avatar-christian.jpg",
     name: "Cristian Duncan",
     role: "Co-founder & COO",
     icon: "../assets/icon-cross.svg",
+    quote:
+      "Distributed teams required unique processes. You need to approach work in a new way.",
+    twitterIcon: "../assets/icon-twitter.svg",
+    linkedinIcon: "../assets/icon-linkedin.svg",
   },
   {
     image: "../assets/avatar-cruz.jpg",
     name: "Cruz Hamer",
     role: "Co-founder & CTO",
     icon: "../assets/icon-cross.svg",
+    quote:
+      "Technology is at the forefront of enabling distributed teams. That's where we come in.",
+    twitterIcon: "../assets/icon-twitter.svg",
+    linkedinIcon: "../assets/icon-linkedin.svg",
   },
   {
     image: "../assets/avatar-drake.jpg",
     name: "Drake Heaton",
     role: "Business Development Lead",
     icon: "../assets/icon-cross.svg",
+    quote:
+      "Hiring similar people from similar backgrounds is a surefire way to stunt innovation.",
+    twitterIcon: "../assets/icon-twitter.svg",
+    linkedinIcon: "../assets/icon-linkedin.svg",
   },
   {
     image: "../assets/avatar-griffin.jpg",
     name: "Griffin Wise",
     role: "Lead Marketing",
     icon: "../assets/icon-cross.svg",
+    quote:
+      "Unique perspectives shape unique products, which is what you need to survive these days.",
+    twitterIcon: "../assets/icon-twitter.svg",
+    linkedinIcon: "../assets/icon-linkedin.svg",
+  },
+  {
+    image: "../assets/avatar-aden.jpg",
+    name: "Aden Allan",
+    role: "Head of Talent",
+    icon: "../assets/icon-cross.svg",
+    quote:
+      "Empowered teams create truly amazing products. Set the north star and let them follow it.",
+    twitterIcon: "../assets/icon-twitter.svg",
+    linkedinIcon: "../assets/icon-linkedin.svg",
   },
 ];
 
@@ -49,7 +79,12 @@ const renderCards = (arr) => {
                 </div>
                 </div>
                 <div class="director__card--backside">
-                <h1>test</h1>
+                <h1 class="director__card--backTitle">${card.name}</h1>
+                <p class="director__card--backQuote">${card.quote}</p>
+                <div class="director__card--backIcons">
+                <img class="director__card--backIcon" src="${card.twitterIcon}" alt="twitter"/>
+                <img class="director__card--backIcon" src="${card.linkedinIcon}" alt="linked"/>
+                </div>
                 </div>
                 </div>
         
